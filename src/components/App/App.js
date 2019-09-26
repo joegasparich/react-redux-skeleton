4a707196b05247f73acccb12a45c5a4829c442ce
Joe Gasparich <joe.gasparich@accedo.tv>
2019-09-27 11:46:25 +1200

    Initial Commit

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import { hot } from "react-hot-loader/root";
+import React, { Component} from "react";
+
+import "./App.scss";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <h1>Hello, World!</h1>
+            </div>
+        );
+    }
+}
+
+export default hot(App);
